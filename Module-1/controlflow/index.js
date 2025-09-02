e1ba634fe43748ee876a3a7ea672af974d1b8351
Husnain <husnainali41940@gmail.com>
2025-09-02 01:48:01 -0700

    Controlflow in js

---
+++ Module-1/controlflow/index.js
@@ -0,0 +1,22 @@
+// Javascript Conntrol flow table:
+// | Tool                       | Use                        |
+// | -------------------------- | -------------------------- |
+// | `if`, `else if`, `else`    | Make decisions             |
+// | `switch`                   | Handle multiple conditions |
+// | `for`, `while`, `do while` | Repeat code (loops)        |
+// | `break`, `continue`        | Control loops              |
+// | `return`                   | Exit a function early      |
+
+// 1) if else :
+let age = 18;
+
+if (age < 18) {
+  console.log("You are underage.");
+} else if (age === 18) {
+  console.log("Just turned adult!");
+} else {
+  console.log("You are an adult.");
+}
+
+
+
